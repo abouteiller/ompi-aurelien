fce79759f7dcb58efa19b4948e6b66ada9807bb1
Aurélien Bouteiller <bouteill@icl.utk.edu>
2016-05-06 17:03:20 -0400

    This hack has been committed by mistake

--- opal/mca/btl/ugni/btl_ugni_frag.h
+++ opal/mca/btl/ugni/btl_ugni_frag.h
@@ -194,7 +194,7 @@ static inline bool mca_btl_ugni_frag_del_ref (mca_btl_ugni_base_frag_t *frag, in
 
     ref_cnt = OPAL_THREAD_ADD32(&frag->ref_cnt, -1);
     if (ref_cnt) {
-        //assert (ref_cnt > 0);
+        assert (ref_cnt > 0);
         return false;
     }
 
