4b01a5764869dff4f922903283053784f5a42301
Aurelien Bouteiller <bouteill@icl.utk.edu>
2019-11-08 17:56:37 -0500

    Bugfix: we need to check if the request if ok before entering the first waitsync_mt
    Signed-off-by: Aurelien Bouteiller <bouteill@icl.utk.edu>

--- ompi/request/req_ft.c
+++ ompi/request/req_ft.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2004-2010 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
  *                         Corporation.  All rights reserved.
- * Copyright (c) 2004-2018 The University of Tennessee and The University
+ * Copyright (c) 2004-2019 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * Copyright (c) 2004-2008 High Performance Computing Center Stuttgart,
@@ -31,6 +31,7 @@
 #include "ompi/mca/pml/base/pml_base_request.h"
 #include "ompi/mca/pml/pml.h"
 
+#include "opal/mca/backtrace/backtrace.h"
 
 /**************************
  * Support Routines
@@ -74,9 +75,28 @@ bool ompi_request_state_ok(ompi_request_t *req)
         req->req_status.MPI_ERROR  = MPI_ERR_REVOKED;
 
         opal_output_verbose(10, ompi_ftmpi_output_handle,
-                            "%s ompi_request_state_ok: %p Communicator %s(%d) has been revoked!",
-                            OMPI_NAME_PRINT(OMPI_PROC_MY_NAME), (void*)req,
+                            "%s ompi_request_state_ok: %p (peer %d, tag %d) is on communicator %s(%d) that has been revoked!",
+                            OMPI_NAME_PRINT(OMPI_PROC_MY_NAME), (void*)req, req->req_peer, req->req_tag,
                             req->req_mpi_object.comm->c_name, req->req_mpi_object.comm->c_contextid);
+        goto return_with_error;
+    }
+
+    /*
+     * Collectives Check:
+     * If the request is part of a collective, then the whole communicator
+     * must be ok to continue. If not then return first failed process
+     */
+    if( OPAL_UNLIKELY(ompi_comm_coll_revoked(req->req_mpi_object.comm) &&
+                      ompi_request_tag_is_collective(req->req_tag)) ) {
+        /* Return the last process known to have failed, may not have been the
+         * first to cause the collectives to be disabled.
+         */
+        req->req_status.MPI_SOURCE = req->req_peer;
+        req->req_status.MPI_ERROR  = MPI_ERR_PROC_FAILED;
+
+        opal_output_verbose(10, ompi_ftmpi_output_handle,
+                            "%s ompi_request_state_ok: Request %p (peer %d) is part of a collective (tag %d), and some process died. (mpi_source %3d)",
+                            OMPI_NAME_PRINT(OMPI_PROC_MY_NAME), (void*)req, req->req_peer, req->req_tag, req->req_status.MPI_SOURCE );
         goto return_with_error;
     }
 
@@ -97,8 +117,8 @@ bool ompi_request_state_ok(ompi_request_t *req)
             }
             req->req_any_source_pending = true;
             opal_output_verbose(10, ompi_ftmpi_output_handle,
-                                "%s ompi_request_state_ok: Request %p in comm %s(%d) peer ANY_SOURCE %s!",
-                                OMPI_NAME_PRINT(OMPI_PROC_MY_NAME), (void*)req,
+                                "%s ompi_request_state_ok: Request %p (peer %d, tag %d) in comm %s(%d) peer ANY_SOURCE %s!",
+                                OMPI_NAME_PRINT(OMPI_PROC_MY_NAME), (void*)req, req->req_peer, req->req_tag,
                                 req->req_mpi_object.comm->c_name, req->req_mpi_object.comm->c_contextid,
                                 ompi_mpi_errnum_get_string(req->req_status.MPI_ERROR));
             goto return_with_error;
@@ -115,44 +135,32 @@ bool ompi_request_state_ok(ompi_request_t *req)
         }
 
         opal_output_verbose(10, ompi_ftmpi_output_handle,
-                            "%s ompi_request_state_ok: Request %p in comm %s(%d) peer %3d failed - Ret %s",
-                            OMPI_NAME_PRINT(OMPI_PROC_MY_NAME), (void*)req,
+                            "%s ompi_request_state_ok: Request %p (peer %d, tag %d) in comm %s(%d) mpi_source %3d failed - Ret %s",
+                            OMPI_NAME_PRINT(OMPI_PROC_MY_NAME), (void*)req, req->req_peer, req->req_tag,
                             req->req_mpi_object.comm->c_name, req->req_mpi_object.comm->c_contextid,
                             req->req_status.MPI_SOURCE,
                             ompi_mpi_errnum_get_string(req->req_status.MPI_ERROR));
         goto return_with_error;
     }
 
-    /*
-     * Collectives Check:
-     * If the request is part of a collective, then the whole communicator
-     * must be ok to continue. If not then return first failed process
-     */
-    if( OPAL_UNLIKELY(ompi_comm_coll_revoked(req->req_mpi_object.comm) &&
-                      ompi_request_tag_is_collective(req->req_tag)) ) {
-        /* Return the last process known to have failed, may not have been the
-         * first to cause the collectives to be disabled.
-         */
-        req->req_status.MPI_SOURCE = req->req_peer;
-        req->req_status.MPI_ERROR  = MPI_ERR_PROC_FAILED;
-
-        opal_output_verbose(10, ompi_ftmpi_output_handle,
-                            "%s ompi_request_state_ok: Request is part of a collective, and some process died. (rank %3d)",
-                            OMPI_NAME_PRINT(OMPI_PROC_MY_NAME), req->req_status.MPI_SOURCE );
-        goto return_with_error;
-    }
+    OPAL_OUTPUT_VERBOSE((100, ompi_ftmpi_output_handle,
+                         "%s ompi_request_state_ok: Request %p (peer %d tag %d) is still ok. (mpi_source %3d)",
+                         OMPI_NAME_PRINT(OMPI_PROC_MY_NAME), (void*)req, req->req_peer, req->req_tag, req->req_status.MPI_SOURCE ));
 
     return true;
 
  return_with_error:
     if( MPI_ERR_PROC_FAILED_PENDING != req->req_status.MPI_ERROR ) {
         int cancelled = req->req_status._cancelled;
-#if 0
-        { int btsize=32; void*bt[32]={NULL}; btsize=backtrace(bt,btsize);
-          backtrace_symbols_fd(bt,btsize, ompi_ftmpi_output_handle);
+#if OPAL_ENABLE_DEBUG
+        int verbose = opal_output_get_verbosity(ompi_ftmpi_output_handle);
+        if( verbose > 90 ) {
+            opal_backtrace_print(stderr, NULL, 1);
         }
-        MCA_PML_CALL(dump(req->req_mpi_object.comm, ompi_ftmpi_output_handle));
-#endif
+        if( verbose > 50 ) {
+            MCA_PML_CALL(dump(req->req_mpi_object.comm, ompi_ftmpi_output_handle));
+        }
+#endif /* OPAL_ENABLE_DEBUG */
         /* Cancel and force completion immmediately
          * However, for Revoked and Collective error we can't complete
          * with an error before the buffer is unpinned (i.e. the request gets
