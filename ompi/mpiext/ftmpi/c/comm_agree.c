67caa594743e642c6459b601d4d84181ebe36bb8
Aurelien Bouteiller <bouteill@icl.utk.edu>
2023-09-21 15:22:12 -0400

    WIP: An AGREE api where the operation computes a uniform num_failed and returns it to the end-user.
    Signed-off-by: Aurelien Bouteiller <bouteill@icl.utk.edu>

--- ompi/mpiext/ftmpi/c/comm_agree.c
+++ ompi/mpiext/ftmpi/c/comm_agree.c
@@ -60,3 +60,40 @@ int MPIX_Comm_agree(MPI_Comm comm, int *flag)
     OMPI_ERRHANDLER_RETURN(rc, comm, rc, FUNC_NAME);
 }
 
+
+int MPIX_Comm_agree_failed(int *flag, int *num_agreed_failed, MPI_Comm comm)
+{
+    int rc = MPI_SUCCESS;
+
+    /* Argument checking */
+    if (MPI_PARAM_CHECK) {
+        OMPI_ERR_INIT_FINALIZE(FUNC_NAME);
+        if (ompi_comm_invalid(comm)) {
+            return OMPI_ERRHANDLER_INVOKE(MPI_COMM_WORLD, MPI_ERR_COMM, FUNC_NAME);
+        }
+        if (NULL == flag) {
+            rc = MPI_ERR_ARG;
+        }
+        OMPI_ERRHANDLER_CHECK(rc, comm, rc, FUNC_NAME);
+    }
+
+    ompi_group_t* failed_group;
+    opal_mutex_lock(&ompi_group_afp_mutex);
+    ompi_group_intersection(comm->c_remote_group, ompi_group_all_failed_procs, failed_group);
+    opal_mutex_unlock(&ompi_group_afp_mutex);
+
+    do {
+        rc = comm->c_coll->coll_agree( flag,
+                                   1,
+                                   &ompi_mpi_int.dt,
+                                   &ompi_mpi_op_band.op,
+                                   &failed_group, true,
+                                   (ompi_communicator_t*)comm,
+                                   comm->c_coll->coll_agree_module);
+    } while(MPI_ERR_PROC_FAILED == rc);
+    *num_agreed_failed = ompi_group_size(failed_group);
+    OBJ_RELEASE( failed_group );
+
+    OMPI_ERRHANDLER_RETURN(rc, comm, rc, FUNC_NAME);
+}
+
